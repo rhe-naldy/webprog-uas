33e4b4ef48ada43aa23c16329f8690338545133e
rhenaldy <45966986+rhe-naldy@users.noreply.github.com>
2023-02-06 19:54:14 +0700

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Welcome</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </head>
 @include('layout.header')
 <body>
-
+    <h1 class="pt-5 d-flex justify-content-center">Find and Buy Your Groceries Here!</h1>
+    <h4 class="pt-5 d-flex justify-content-center">Please Register/Login to use our service</h4>
 </body>
 @include('layout.footer')
 </html>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,30 +8,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </head>
-<body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary p-3">
-        <a class="navbar-brand mb-1 mx-2 h1" href="#">Amazing E-Grocery</a>
-        <div class="collapse navbar-collapse justify-content-between">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/cart">Cart</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">Profile</a>
-                </li>
-            </ul>
-            <div>
-                @if (!Auth::check())
-                    <button class="btn btn-danger" type="button" onclick="window.location='/register'">Register</button>
-                    <button class="btn btn-primary" type="button" onclick="window.location='/login'">Login</button>
-                @elseif(Auth::check())
-                    <button class="btn btn-danger" type="button" onclick="window.location='/logout'">Logout</button>
-                @endif
-            </div>
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary p-3">
+    <a class="navbar-brand mb-1 mx-2 h1" href="#">Amazing E-Grocery</a>
+    <div class="collapse navbar-collapse justify-content-between">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/cart">Cart</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/profile">Profile</a>
+            </li>
+        </ul>
+        <div>
+            @if (!Auth::check())
+                <button class="btn btn-danger" type="button" onclick="window.location='/register'">Register</button>
+                <button class="btn btn-primary" type="button" onclick="window.location='/login'">Login</button>
+            @elseif(Auth::check())
+                <button class="btn btn-danger" type="button" onclick="window.location='/logout'">Logout</button>
+            @endif
         </div>
-    </nav>
-</body>
+    </div>
+</nav>
 </html>
