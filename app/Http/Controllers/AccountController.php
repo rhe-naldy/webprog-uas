ba7b7d47390a8f9d5211044f6fd6e1b0804b3165
rhenaldy <45966986+rhe-naldy@users.noreply.github.com>
2023-02-06 19:36:03 +0700

    index & home

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
-    //
+    public function checkAuth(){
+        if(Auth::check()){
+            return view('home');
+        } else if(!Auth::check()){
+            return view('index');
+        }
+    }
 }
