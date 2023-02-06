8ef49b7632caaae406cb736aa74a2b14ea862669
rhenaldy <45966986+rhe-naldy@users.noreply.github.com>
2023-02-06 21:25:55 +0700

    Update Account.php

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    use HasFactory;
+    protected $fillable = ['role_id', 'gender_id', 'first_name', 'last_name', 'email', 'display_picture_link', 'password'];
+
+    public function gender(){
+        return $this->belongsTo(Gender::class, "gender_id", "gender_id");
+    }
+
+    public function role(){
+        return $this->belongsTo(Role::class, "role_id", "role_id");
+    }
 }
