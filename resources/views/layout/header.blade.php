930c2aa20ce68e5fbe8ec72ec3cffc4bf783f98f
rhenaldy <45966986+rhe-naldy@users.noreply.github.com>
2023-02-07 23:35:55 +0700

    footer headder

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-</head>
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary p-3 mb-5">
     <a class="navbar-brand mb-1 mx-2 h1" href="/">Amazing E-Grocery</a>
     <div class="collapse navbar-collapse justify-content-between">
@@ -32,4 +22,3 @@
         </div>
     </div>
 </nav>
-</html>
