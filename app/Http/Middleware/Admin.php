0bd0c4b1614b5ea878e3adc8abbdd342791c7c9e
rhenaldy <45966986+rhe-naldy@users.noreply.github.com>
2023-02-08 17:00:19 +0700

    middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -18,7 +18,7 @@ class Admin
     public function handle(Request $request, Closure $next)
     {
         if(!Auth::check() || Auth::user()->role->role_id != "admin"){
-            return abort(401);
+            return redirect('/home');
         }
         return $next($request);
     }
